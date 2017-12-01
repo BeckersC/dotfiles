a0e5ef481ec83ea5ba3f3843f6d8059972998ffb
BeckersC <beckers.christian@protonmail.com>
2017-12-01 16:51:41 +0100

    switch monitor side

--- .config/i3/scripts/screenlayout.sh
+++ .config/i3/scripts/screenlayout.sh
@@ -21,9 +21,10 @@ done
 config_file=~/.config/i3/config.d/0_Screens
 rm $config_file
 touch $config_file
-echo "#Screen Variables" >> $config_file
-echo $left >> $config_file
-echo $right >> $config_file
+echo '#Screen Variables' >> $config_file
+echo 'set $screen1' $right >> $config_file
+echo 'set $screen2' $left >> $config_file
+echo '' >> $config_file
 
 echo xrandr --output $left --primary --auto --pos 0x0 --rotate $orientation --output $right --auto --right-of $left --rotate normal 
 xrandr --output $left --primary --auto --pos 0x0 --rotate $orientation --output $right --auto --right-of $left --rotate normal 
