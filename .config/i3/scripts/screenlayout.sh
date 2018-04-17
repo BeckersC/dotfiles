e5a403a9dde6897c1a2cf98203a3f9fd45dc8cd5
Create4Life <beckers.christian@protonmail.com>
2018-04-17 04:28:20 +0200

    fix Variable name - Add descriptive comments

--- .config/i3/scripts/screenlayout.sh
+++ .config/i3/scripts/screenlayout.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+config_file=~/.config/i3/config.d/0_Screens
+
 #Mainscreen
 Mainscreen="HDMI"
 MainRelPositionTo="--right-of"
@@ -16,11 +18,13 @@ SecondaryOffset="0"
 #position: --left-of --right-of --above --below --same-as (other output)
 #orientation: normal/inverted/left/right
 
+echo "Connected Monitors are: "
+
 #Main Screen
 for output in $(xrandr | grep '\Wconnected'| awk '{ print $1 }'); do
     if [[ $output =~ $Mainscreen ]] ; then
         Main=$output
-        echo "$output= " $Main
+        echo "Main = "$Main
     fi
 done
 
@@ -28,18 +32,23 @@ done
 for output in $(xrandr | grep '\Wconnected' | awk '{ print $1 }'); do
     if [[ ! $output =~ $Mainscreen ]] ; then
         Secondary=$output
-        echo "right = " $Secondary
+        echo "Secondary = "$Secondary
     fi
 done
 
-config_file=~/.config/i3/config.d/0_Screens
+echo ""
+
 rm $config_file
 touch $config_file
-echo '#Screen Variables' >> $config_file
 echo 'set $screen1' $Main>> $config_file
 echo 'set $screen2' $Secondary>> $config_file
 echo '' >> $config_file
 
-echo xrandr --output $Main --primary --auto $RelPositionTo $Secondary --rotate $Orientation
-xrandr --output $Main --primary --auto $RelPositionTo $Secondary --rotate $Orientation
-xrandr --output $Secondary --auto Secondary --rotate $Orientation
+echo "configuring i3 config > $config_file"
+cat $config_file
+
+echo "configuring xrandr displays"
+echo xrandr --output $Main --primary --auto $MainRelPositionTo $Secondary --rotate $MainOrientation
+xrandr --output $Main --primary --auto $MainRelPositionTo $Secondary --rotate $MainOrientation
+echo xrandr --output $Secondary --auto $SecondaryRelPositionTo $Main --rotate $SecondaryOrientation
+xrandr --output $Secondary --auto $SecondaryRelPositionTo $Main --rotate $SecondaryOrientation
