e365b08db6ab83617227491ca60c4c2e8a8ec9f1
Create4Life <beckers.christian@protonmail.com>
2018-04-17 04:12:31 +0200

    rewrite automatic screen config | more robust

--- .config/i3/scripts/screenlayout.sh
+++ .config/i3/scripts/screenlayout.sh
@@ -1,22 +1,34 @@
 #!/bin/bash
 
-screenleft="HDMI*"
-orientation="left"
-offset="400"
+#Mainscreen
+Mainscreen="HDMI"
+MainRelPositionTo="--right-of"
+MainOrientation="normal"
+MainOffset="0"
 
-left="screenleft"
+#Secondary Screen
+
+SecondaryRelPositionTo="--left-of"
+SecondaryOrientation="normal"
+SecondaryOffset="0"
+
+#Variables
+#position: --left-of --right-of --above --below --same-as (other output)
+#orientation: normal/inverted/left/right
+
+#Main Screen
 for output in $(xrandr | grep '\Wconnected'| awk '{ print $1 }'); do
-    if [[ $output =~ $screenleft ]] ; then
-        left=$output
-        echo "left = " $left
+    if [[ $output =~ $Mainscreen ]] ; then
+        Main=$output
+        echo "$output= " $Main
     fi
 done
 
-right="left"
+#Secondary Screen
 for output in $(xrandr | grep '\Wconnected' | awk '{ print $1 }'); do
-    if [[ ! $output =~ $screenleft ]] ; then
-        right=$output
-        echo "right = " $right
+    if [[ ! $output =~ $Mainscreen ]] ; then
+        Secondary=$output
+        echo "right = " $Secondary
     fi
 done
 
@@ -24,9 +36,10 @@ config_file=~/.config/i3/config.d/0_Screens
 rm $config_file
 touch $config_file
 echo '#Screen Variables' >> $config_file
-echo 'set $screen1' $right >> $config_file
-echo 'set $screen2' $left >> $config_file
+echo 'set $screen1' $Main>> $config_file
+echo 'set $screen2' $Secondary>> $config_file
 echo '' >> $config_file
 
-echo xrandr --output $left --primary --auto --pos 0x0 --rotate $orientation --output $right --auto --right-of $left --rotate normal 
-xrandr --output $left --primary --auto --pos 0x0 --rotate $orientation --output $right --auto --right-of $left --rotate normal 
+echo xrandr --output $Main --primary --auto $RelPositionTo $Secondary --rotate $Orientation
+xrandr --output $Main --primary --auto $RelPositionTo $Secondary --rotate $Orientation
+xrandr --output $Secondary --auto Secondary --rotate $Orientation
